7f0fda5335e12dbd28ccd5638c7f9bc3bd3bd480
awaw00 <awaw0618@outlook.com>
2016-03-31 11:12:27 +0800

    【修改】重复代码

--- generators/blueprint/index.js
+++ generators/blueprint/index.js
@@ -35,7 +35,7 @@ module.exports = generators.Base.extend({
       name,
       classifyName: Inflector.classify(name),
       titleizeName: Inflector.titleize(name),
-      underscoreName: Inflector.underscore(name)
+      underscoreName: fileName
     }
     var all = type === 'all'
     if (type === 'form' || all) {
